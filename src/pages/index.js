98e3a947adbac8cdb48d31b6fd3beb5c9d68f0cd
IvanGonzalez10 <ivan.c.gonzalez1@gmail.com>
2020-11-08 14:16:51 -0500

    Boton de comprar

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link, graphql } from "gatsby";
+import styled from 'styled-components';
 
 import Jumbo from "../components/jumbo";
 import Image from "../components/image";
@@ -19,18 +20,25 @@ query GET_DESCRPTION{
 }
 `
 
+const Button = styled.button `
+  width: 8rem;
+  background-color: #98ca3f;
+  border: none;
+  border-radius: 10px;
+  color: ${props => props.color};
+  &:hover{
+    transform: scale(1.2)
+  }
+`
+
 const IndexPage = ({data}) => (
   <>
     <SEO title="Home" />
     <Jumbo description={data.allSite.edges[0].node.siteMetadata.description}/>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link> <br />
+    <Button color='gray'>Comprar</Button>
     <Link to="/gracias/">Go to gracias</Link><br />
     <Link to="/cancelacion/">Go to cancelacion</Link>
   </>
